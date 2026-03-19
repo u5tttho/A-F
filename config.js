3e57e7ad569f9a07ea59f7658a68e640d66215a8
u5tttho <abdullahhawsawi0077x++@gmail.com>
2026-03-19 23:56:35 +0300

    Update and rename main.py to config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  token: "BOT_TOKEN_HERE",
+  prefix: "!",
+  ownerId: "YOUR_DISCORD_ID",
+  colors: {
+    main: "#5865F2",
+    success: "#57F287",
+    error: "#ED4245",
+    warning: "#FEE75C",
+    info: "#00B0F4"
+  }
+};
